b2e78ef4886b537848391ab97fff06afe3063e49
JoshuaMumo <jmumo6672@gmail.com>
2025-05-30 17:30:18 +0300

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "583d5d4392c6d02d5fe3d462e5694d56";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
